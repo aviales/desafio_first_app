18419987bb47da00283b4bdc06db338f0b006b04
alonso <avialesarrieta@gmail.com>
2021-02-25 13:42:26 -0600

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages', to: 'pages#one'
   get 'pages', to: 'pages#two'
+  get 'pages', to: 'pages#three'
   root 'pages#one'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
